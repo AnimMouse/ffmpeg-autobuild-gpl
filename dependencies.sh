0ef78c53f5182ec7265f64d482c8958dd03c3668
AnimMouse <git@animmouse.com>
2022-10-01 01:46:37 +0800

    APT quiet level 2 implies -y

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get -qq -y update
-sudo apt-get -qq -y upgrade
-sudo apt-get -qq -y install ragel cvs yasm pax nasm gperf autogen autoconf-archive
+sudo apt-get -qq update
+sudo apt-get -qq upgrade
+sudo apt-get -qq install ragel cvs yasm pax nasm gperf autogen autoconf-archive
 sudo -H pip3 -qq install meson ninja
